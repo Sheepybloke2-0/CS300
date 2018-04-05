aed1b949c41a3961f510a2c4985f72d7d2f1a80a
Reuben <rtl55@outlook.com>
2018-04-04 20:39:17 -0400

    Fixed issue with the system appending 0s onto the back of the icAddress.
    Now working perfectly with MQTT!

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -16,37 +16,34 @@ QOS = 0
 icAddress = bin(icAddress)[2:]
 icAddress = icAddress.zfill(7)
 icAddress = [int(x) for x in icAddress]
+icAddress.append(0)
 
 registerAddress = bin(registerAddress)[2:]
 registerAddress = registerAddress.zfill(8)
 registerAddress = [int(x) for x in registerAddress]
+
 
 def getTemperature():
     #Local Constants
     global SCL, SDA, icAddress, registerAddress
     GPIO.setup(SCL, GPIO.OUT)
     sendStart()
+   
     #Write icAddress w/ WRITE Bit
-    icAddress.append(0)
-    writeData(icAddress)
-    #if(writeData(icAddress)):
-        #print("Address Ack for initial write from Sensor")
-    #else:
-        #print("Address not Acknowledged by Sensor for initial write !!")
-    writeData(registerAddress)
-    #if(writeData(registerAddress)):
-        #print("Register Ack from Sensor")
-    #else:
-        #print("Register not Acknowledged by Sensor!!")
+    icAddress[7] = 0
+    if(writeData(icAddress) == False):
+        print("Address not Acknowledged by Sensor for initial write !!")
+    
+    if(writeData(registerAddress) == False):
+        print("Register not Acknowledged by Sensor!!")
 
     sendStart()
+
     #Change R/W bit to Read
     icAddress[7] = 1
-    writeData(icAddress)
-    #if(writeData(icAddress)):
-        #print("Address Ack for Data Read from Sensor")
-    #else:
-        #print("Address not Acknowledged by Sensor for Data Read !!")
+    if(writeData(icAddress) == False):
+        print("Address not Acknowledged by Sensor for Data Read !!")
+    
     msbData = readData()
     sendACK()
     lsbData = readData()
@@ -66,8 +63,7 @@ def getTemperature():
         Temperature = 256 - (UpperByte * 16 + LowerByte / 16)
     else:
         Temperature = (UpperByte * 16 + LowerByte / 16)
-    #print("Temperature in Celsius: ", Temperature)
-    #print("Temperature in Farenheit: ", (Temperature * 9/5) + 32)
+    print("Temperature in Celsius: ", Temperature)
     return Temperature
 
 #Start Sequence
@@ -110,10 +106,10 @@ def readData():
         GPIO.output(SCL, True)
         time.sleep(0.001)
         readByte.append(GPIO.input(SDA))
-        #print(readByte)
         GPIO.output(SCL, False)
         time.sleep(0.001)
     return readByte
+
 def sendACK():
     GPIO.setup(SDA, GPIO.OUT)
     GPIO.output(SDA, False)
@@ -151,33 +147,28 @@ def on_publish(client, userdata, mid):
 def on_connect(client, userdata, rc, *extra_params):
  print('Connected with result code='+str(rc))
 
-# Callback when client receives a PUBLISH message from the broker
-def on_message(client, data, msg):
- if msg.topic == "rtl5/ipaddress":
-  print("My IP address is:   " + str(msg.payload))
+if __name__ == '__main__':
+ # Setup MQTT client
+ client = mqtt.Client()
+ client.on_connect = on_connect
+ client.on_publish = on_publish
 
-# Setup MQTT client
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.on_publish = on_publish
+ # Connect to MQTT broker and subscribe to the button topic
+ client.connect(BROKER, PORT, 60)
 
-# Connect to MQTT broker and subscribe to the button topic
-client.connect(BROKER, PORT, 60)
-client.subscribe("dwm5/temperature", qos=QOS)
-client.loop_start()
+ while (True):
+  try:
+   message = getTemperature()
+   # Publish message to broker
+   (result, num) = client.publish("dwm5/temperature", message, qos=0, retain=True)
+   if result != 0:
+    print("PUBLISH returned error:", result)
+     
+   time.sleep(2)
 
+  except KeyboardInterrupt:
+    break
 
-while (True):
-    message = getTemperature()
-    # Publish message to broker
-    (result, num) = client.publish("dwm5/temperature", message, qos=0, retain=True)
-    if result != 0:
-        print("PUBLISH returned error:", result)
+ client.disconnect()
 
-    time.sleep(2)
-
-client.disconnect()
-
-
-GPIO.cleanup()
+ GPIO.cleanup()
